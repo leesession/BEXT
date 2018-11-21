82fdca33777f2c48050cee21bff63011f9a19eee
Chris Li <chris.li.2046@gmail.com>
2018-11-21 15:33:04 -0800

    Fix build/files not found by nginx problem

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,8 +1,10 @@
 # back up existing file
 cp -f /etc/nginx/sites-available/default /etc/nginx/sites-available/bak_default
 
+[ -f ./scripts/config-nginx ] && echo "Found ./scripts/config-nginx" || echo "Not found config-nginx under ./scripts, are you at betx-fe root?"
+
 # Copy config-nginx over
-cp -f ./config-nginx /etc/nginx/sites-available/default
+cp -f ./scripts/config-nginx /etc/nginx/sites-available/default
 
 sudo nginx -t 
 sudo service nginx restart
