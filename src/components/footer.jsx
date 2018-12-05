ddaf9546a52b640d90b1819c78a721fed05a32b9
Chris Li <chris.li.2046@gmail.com>
2018-12-04 22:59:40 -0800

    Fixed a problem where footer wechat image doesn't show

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,44 +8,65 @@ import { cloudinaryConfig, CloudinaryImage } from './react-cloudinary';
 cloudinaryConfig({ cloud_name: 'forgelab-io' });
 
 class Footer extends React.Component {
-  render() {
-    const { intl } = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
 
-    return (
-      <div id="footer">
-        <div className="contact-container">
-
-          <ul>
-            <li>
-              <a href={`mailto:${siteConfig.contactEmail}`}>
-                <i className="fa fa-envelope-o" />
-              </a>
-            </li>
-            <li>
-              <a href={siteConfig.telegramEN}>
-                <i className="fa fa-paper-plane-o" />
-              </a>
-            </li>
-            <li>
-              <a href={siteConfig.twitter} target="_blank">
-                <i className="fa fa-twitter" />
-              </a>
-            </li>
-            <li>
-              <Popover content={<CloudinaryImage publicId="qr-wechat-betx" options={{ height: 150, crop: 'scale' }} alt="QR code Wechat" />} title={intl.formatMessage({ id: 'footer.contactus.wechat' })}>
-                <span><i className="fa fa-wechat" /></span>
-              </Popover>
-            </li>
-          </ul>
-
-        </div>
-
-        <div className="corp-container">
-          <span>{siteConfig.footerText}</span>
-        </div>
-      </div>
-    );
+    this.handleVisibleChange = this.handleVisibleChange.bind(this);
   }
+
+   handleVisibleChange = (visible) => {
+     this.setState({ visible });
+   }
+
+   render() {
+     const { intl } = this.props;
+     const { visible } = this.state;
+
+     return (
+       <div id="footer">
+         <div className="contact-container">
+
+           <ul>
+             <li>
+               <a href={`mailto:${siteConfig.contactEmail}`}>
+                 <i className="fa fa-envelope-o" />
+               </a>
+             </li>
+             <li>
+               <a href={siteConfig.telegramEN}>
+                 <i className="fa fa-paper-plane-o" />
+               </a>
+             </li>
+             <li>
+               <a href={siteConfig.twitter} target="_blank">
+                 <i className="fa fa-twitter" />
+               </a>
+             </li>
+             <li>
+               <Popover
+                 placement="top"
+                 content={<CloudinaryImage publicId="qr-wechat-betx" options={{ height: 150, crop: 'scale' }} alt="QR code Wechat" />}
+                 title={intl.formatMessage({ id: 'footer.contactus.wechat' })}
+                 trigger="click"
+                 visible={visible}
+                 onVisibleChange={this.handleVisibleChange}
+               >
+                 <span><i className="fa fa-wechat" /></span>
+               </Popover>
+             </li>
+           </ul>
+
+         </div>
+
+         <div className="corp-container">
+           <span>{siteConfig.footerText}</span>
+         </div>
+       </div>
+     );
+   }
 }
 
 Footer.propTypes = {
