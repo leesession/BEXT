672ac798bef13e8aa9af8874babf5851aa4f2561
Chris Li <chris.li.2046@gmail.com>
2018-12-16 03:26:34 -0800

    Updated pm2 scripts

--- scripts/pm2.sh
+++ scripts/pm2.sh
@@ -4,6 +4,6 @@ git checkout ubuntu-16.04
 git rebase master
 export GENERATE_SOURCEMAP=false && yarn build
 yarn global add serve
-pm2 stop BETX-FE
-pm2 start serve --name BETX-FE -- -s build
+pm2 stop serve
+pm2 start serve -- -s build
 pm2 list
