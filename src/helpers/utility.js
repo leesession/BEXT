49e4981bcbac8c2fd89fca47260a06478652e467
Chris Li <chris.li.2013@hotmail.com>
2019-01-11 15:47:04 -0800

    Fixed utility thousand delimiter not showing correctly problem

--- src/helpers/utility.js
+++ src/helpers/utility.js
@@ -125,15 +125,22 @@ export function millisecToMin(millis) {
 
 /**
  * Formatting number with thousand separator.
- * @param  {number} x e.g. 1000000.65
+ * @param  {number} num e.g. 1000000.65
  * @return {string}   "1,000,000.65"
  */
-export function formatNumberThousands(x) {
-  if (_.isUndefined(x)) {
-    return x;
+export function formatNumberThousands(num) {
+  if (_.isUndefined(num)) {
+    return num;
   }
 
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  const numStr = num.toString();
+  const parts = numStr.split('.');
+
+  const decimalStr = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  const period = _.isUndefined(parts[1]) ? '' : '.';
+  const floatStr = _.isUndefined(parts[1]) ? '' : parts[1];
+
+  return `${decimalStr}${period}${floatStr}`;
 }
 
 /**
