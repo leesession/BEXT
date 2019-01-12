6a63e0807d88b715255d0bc12c9ac3ba7bccc7c6
Chris Li <chris.li.2013@hotmail.com>
2019-01-12 00:30:18 -0800

    Remove usused component from helper urlsync

--- src/helpers/urlSync.js
+++ src/helpers/urlSync.js
@@ -19,7 +19,7 @@ export function getDefaultPath() {
   const getParent = (lastRoute) => {
     const parents = {
       ecommerce: ['shop', 'cart', 'checkout', 'card'],
-      map: ['googlemap', 'leafletmap'],
+      map: ['googlemap'],
       charts: [
         'googleChart',
         'reecharts',
