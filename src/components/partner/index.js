c3a834fa2c754862292795e4b9bb59d6ea2b0e50
leesession <1063357624@qq.com>
2019-03-23 18:13:28 +0800

    first initial

--- src/components/partner/index.js
+++ src/components/partner/index.js
@@ -2,20 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Item from 'antd/lib/list/Item';
 import partner from './partner'
-import lianjinshu from '../../image/partner/lianjinshu.png';
-import bitpie from '../../image/partner/bitpie.png';
-import buckWallet from '../../image/partner/buck_wallet.png';
-import dexeo from '../../image/partner/dexeo.svg';
-import findex from '../../image/partner/findex.png';
-import maizi from '../../image/partner/maizi.svg';
-import prochain from '../../image/partner/prochain.png';
-import secrypto from '../../image/partner/secrypto.png';
-import tokenPocket from '../../image/partner/TP.png';
-import dappradar from '../../image/partner/dappradar.png';
-import dappreview from '../../image/partner/dappreview.svg';
-import spiderstore from '../../image/partner/spiderstore.svg';
-import meet from '../../image/partner/meet.png';
-import newdex from '../../image/partner/newdex.png';
+
+import sprit from '../../image/partner/sprit.png'
+
+
 
 class Listdetail extends Component{
     componentDidMount(){
@@ -23,23 +13,36 @@ class Listdetail extends Component{
     }
     render(){
         const partnerArr = [partner.partner_package,partner.partner_message,partner.partner_exchange]
-        const series = ['EOS钱包','EOS信息平台','交易所']
-        const imgArr=[[tokenPocket,meet,bitpie,secrypto,maizi,buckWallet],
-                        [lianjinshu,dappradar,spiderstore,dappreview,prochain,],
-                        [newdex,dexeo,findex]]
+        const series = [
+            { name:'EOS钱包',num:0 }, 
+            { name:'EOS信息平台',num:partnerArr[0].length},
+            {name:'交易所',num:partnerArr[0].length+partnerArr[1].length},
+        ]
+      
         return(
             <div id='part'>
             {
                 series.map((Items,index)=>{
                     return <div className='contain'>
-                                <div className='series'>{Items}</div>
+                                <div className='series'>{Items.name}</div>
                                 <ul>
                                 {
                                     partnerArr[index].map((Item,indexs)=>{
                                         return <li>
                                                      <a href={Item.link} target='_blank'>
                                                           {/* <img src={Item.src} width='70' height='70'/> */}
-                                                          <span style={{backgroundImage:`url(${imgArr[index][indexs]}`}}></span>
+                                                          <span style={{
+                                                              backgroundImage:`url(${sprit}`,
+                                                              backgroundPositionX:-(indexs+Items.num)*70+'px'
+                                                              }}
+                                                              className={
+                                                                  (indexs+Items.num)==3?'logo4':
+                                                                    (indexs+Items.num)==7?'logo8':
+                                                                        (indexs+Items.num)==9?'logo10':
+                                                                            (indexs+Items.num)==11?'logo12':
+                                                                            (indexs+Items.num)==12?'logo13':''
+                                                              }
+                                                              ></span>
                                                           <div>
                                                              <div>{Item.name}</div>
                                                                <div>{Item.intro}</div>
